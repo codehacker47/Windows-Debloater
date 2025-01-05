fd1af336ae96ca161efb96bfeadff0b76fb378bf
codehacker47 <davide@tartaglini.com>
2025-01-05 18:30:15 +0100

    Added options to clear browser's cache
    Option to remove browser cache, but for only Edge, Firefox and Chrome.

--- cleaner.bat
+++ cleaner.bat
@@ -5,7 +5,8 @@ echo Windows Cleaner
 echo 1. Delete files in temporary folders
 echo 2. Disable Windows Telemetry(tested in Windows 11 only)
 echo 3. Optimize C: Disk
-echo 4. Exit
+echo 4. Clear browser cache(Edge, Chrome and Firefox only)
+echo 5. Exit
 
 set /p choice=Choose an option: 
 
@@ -13,6 +14,7 @@ if %choice%==1 goto option1
 if %choice%==2 goto option2
 if %choice%==3 goto option3
 if %choice%==4 goto option4
+if %choice%==5 goto option5
 
 goto menu
 
@@ -65,4 +67,39 @@ pause
 goto menu
 
 :option4
+cls
+echo 1. Google Chrome
+echo 2. Microsoft Edge
+echo 3. Mozilla Firefox
+set /p browser_choice=Choose your browser
+if %browser_choice%==1 goto chrome_cache
+if %browser_choice%==2 goto edge_cache
+if %browser_choice%==3 goto firefox_cache
+
+:chrome_cache
+echo Clearing Google Chrome cache...
+del /q /s /f "%LocalAppData%\Google\Chrome\User Data\Default\Cache\*.*"
+del /q /s /f "%LocalAppData%\Google\Chrome\User Data\Default\Code Cache\*.*"
+del /q /s /f "%LocalAppData%\Google\Chrome\User Data\Default\Media Cache\*.*"
+echo Google Chrome cache cleared!
+pause
+goto menu
+
+:edge_cache
+echo Clearing Microsoft Edge cache...
+del /q /s /f "%LocalAppData%\Microsoft\Edge\User Data\Default\Cache\*.*"
+del /q /s /f "%LocalAppData%\Microsoft\Edge\User Data\Default\Code Cache\*.*"
+del /q /s /f "%LocalAppData%\Microsoft\Edge\User Data\Default\Media Cache\*.*"
+echo Microsoft Edge cache cleared!
+pause
+goto menu
+
+:firefox_cache
+echo Clearing Mozilla Firefox cache...
+del /q /s /f "%AppData%\Mozilla\Firefox\Profiles\*\cache2\*.*"
+echo Mozilla Firefox cache cleared!
+pause
+goto menu
+
+:option5
 Exit
